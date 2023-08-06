dcf18f3d30e693fb39418f0d3c305c59b09b58d3
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-06 15:24:05 +0300

    hopefully inappbrowser setted up

--- src/App.tsx
+++ src/App.tsx
@@ -47,99 +47,111 @@ import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { ENV } from "./env.dev";
 import { HeaderComponent } from "./components/HeaderComponent";
 import { NotFoundComponent } from "./components/NotFoundComponent";
+import { useEffect } from "react";
+import { InAppBrowser } from "@awesome-cordova-plugins/in-app-browser";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <ClerkProvider
-    publishableKey={ENV.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    appearance={{
-      variables: {
-        colorPrimary: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--ion-color-primary"),
-        colorSuccess: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--ion-color-success"),
-        colorDanger: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--ion-color-danger"),
-        colorBackground: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue(
-          window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "--ion-color-dark"
-            : "--ion-color-light"
-        ),
-        colorText: getComputedStyle(document.documentElement).getPropertyValue(
-          window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "--ion-color-light"
-            : "--ion-color-dark"
-        ),
-        colorTextOnPrimaryBackground: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--ion-color-dark"),
-        colorWarning: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--ion-color-warning"),
-        colorAlphaShade: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue(
-          window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "--ion-color-light"
-            : "--ion-color-dark"
-        ),
-      },
-    }}
-  >
-    <IonApp>
-      <IonReactRouter>
-        <SignedIn>
-          <HeaderComponent />
-          <Route path="/" exact>
-            <Redirect to="/tab1" />
-          </Route>
-          <Route path={["/tab1", "/tab2", "/tab3"]}>
-            <IonTabs>
-              <IonRouterOutlet>
-                <Route path="/tab1" exact>
-                  <Tab1 />
-                </Route>
-                <Route path="/tab2" exact>
-                  <Tab2 />
-                </Route>
-                <Route path="/tab3" exact>
-                  <Tab3 />
-                </Route>
-              </IonRouterOutlet>
-              <IonTabBar slot="bottom">
-                <IonTabButton tab="tab1" href="/tab1">
-                  <IonIcon aria-hidden="true" icon={triangle} />
-                  <IonLabel>Tab 1</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab2" href="/tab2">
-                  <IonIcon aria-hidden="true" icon={ellipse} />
-                  <IonLabel>Tab 2</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab3" href="/tab3">
-                  <IonIcon aria-hidden="true" icon={square} />
-                  <IonLabel>Tab 3</IonLabel>
-                </IonTabButton>
-              </IonTabBar>
-            </IonTabs>
-          </Route>
-        </SignedIn>
-        <SignedOut>
-          <Route path={["/", "/redirectTo/:page", "/redirectTo"]} exact>
-            <LandingPage />
-          </Route>
-        </SignedOut>
-      </IonReactRouter>
-    </IonApp>
-  </ClerkProvider>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    document.querySelector("a")?.addEventListener("click", (e) => {
+      e.preventDefault();
+      InAppBrowser.create((e.target as any)?.url, "_blank");
+    });
+  });
+  return (
+    <ClerkProvider
+      publishableKey={ENV.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        variables: {
+          colorPrimary: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue("--ion-color-primary"),
+          colorSuccess: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue("--ion-color-success"),
+          colorDanger: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue("--ion-color-danger"),
+          colorBackground: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue(
+            window.matchMedia &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+              ? "--ion-color-dark"
+              : "--ion-color-light"
+          ),
+          colorText: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue(
+            window.matchMedia &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+              ? "--ion-color-light"
+              : "--ion-color-dark"
+          ),
+          colorTextOnPrimaryBackground: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue("--ion-color-dark"),
+          colorWarning: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue("--ion-color-warning"),
+          colorAlphaShade: getComputedStyle(
+            document.documentElement
+          ).getPropertyValue(
+            window.matchMedia &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+              ? "--ion-color-light"
+              : "--ion-color-dark"
+          ),
+        },
+      }}
+    >
+      <IonApp>
+        <IonReactRouter>
+          <SignedIn>
+            <HeaderComponent />
+            <Route path="/" exact>
+              <Redirect to="/tab1" />
+            </Route>
+            <Route path={["/tab1", "/tab2", "/tab3"]}>
+              <IonTabs>
+                <IonRouterOutlet>
+                  <Route path="/tab1" exact>
+                    <Tab1 />
+                  </Route>
+                  <Route path="/tab2" exact>
+                    <Tab2 />
+                  </Route>
+                  <Route path="/tab3" exact>
+                    <Tab3 />
+                  </Route>
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                  <IonTabButton tab="tab1" href="/tab1">
+                    <IonIcon aria-hidden="true" icon={triangle} />
+                    <IonLabel>Tab 1</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab2" href="/tab2">
+                    <IonIcon aria-hidden="true" icon={ellipse} />
+                    <IonLabel>Tab 2</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab3" href="/tab3">
+                    <IonIcon aria-hidden="true" icon={square} />
+                    <IonLabel>Tab 3</IonLabel>
+                  </IonTabButton>
+                </IonTabBar>
+              </IonTabs>
+            </Route>
+          </SignedIn>
+          <SignedOut>
+            <Route path={["/", "/redirectTo/:page", "/redirectTo"]} exact>
+              <LandingPage />
+            </Route>
+          </SignedOut>
+        </IonReactRouter>
+      </IonApp>
+    </ClerkProvider>
+  );
+};
 
 export default App;
